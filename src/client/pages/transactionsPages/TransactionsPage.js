3af3381cd254efc1b1fd2d1d7e923cd9a6b24d0b
Guusy <guusygonzalo@gmail.com>
2019-06-30 19:18:28 -0300

    add friendly spinner

--- src/client/pages/transactionsPages/TransactionsPage.js
+++ src/client/pages/transactionsPages/TransactionsPage.js
@@ -4,8 +4,9 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import TransactionsAccordion from './components/transactionsAccordion/TransactionsAccordion'
 import TransactionsService from '../../services/TransactionsService'
 import Grid from '@material-ui/core/Grid';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
     root: {
         height: '100vh',
         padding: "2em",
@@ -13,23 +14,30 @@ const useStyles = makeStyles(() => ({
         justifyContent: "center"
 
     },
+    progress: {
+        margin: theme.spacing(2),
+    },
 }));
 const TransactionsPage = () => {
     const classes = useStyles();
     const [transactions, setTransactions] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         async function fetchData() {
             const transactionsResponse = await TransactionsService.getAll();
             setTransactions(transactionsResponse);
+            setIsLoading(false);
         }
         fetchData();
     }, []);
 
-    return (<Grid container component="main" className={classes.root}>
-        <CssBaseline />
-        <TransactionsAccordion transactions={transactions} />
-    </Grid>)
+    return (
+        <Grid container component="main" className={classes.root}>
+            <CssBaseline />
+            {isLoading ? <CircularProgress className={classes.progress} /> : <TransactionsAccordion transactions={transactions} /> }
+            
+        </Grid>)
 }
 
 export default TransactionsPage;
--- src/client/pages/transactionsPages/components/transactionsAccordion/TransactionsAccordion.js
+++ src/client/pages/transactionsPages/components/transactionsAccordion/TransactionsAccordion.js
@@ -11,6 +11,7 @@ export default function TransactionsAccordion(props) {
   const { transactions } = props;
   return (
     <div style={{ width: "100%" }}>
+      <h3> Transactions </h3>
       {transactions.map(transaction => <TransactionPanel key={transaction.id} {...transaction} handleChange={handleChange} expanded={expanded} />)}
       {transactions.length === 0 && <Typography className="empty-message">Sorry, there is no transaction yet</Typography>}
     </div>
